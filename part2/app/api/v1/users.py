cd136de7bb95a24acee484d945c1875c2ade40a0
cvalverde96 <cvalverde96@gmail.com>
2024-10-25 16:35:42 -0400

    subiendo users con modificaciones

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+
 from flask_restx import Namespace, Resource, fields
-from ...services import facade
+from app.services import facade
+import uuid
 
 api = Namespace('users', description='User operations')
 
@@ -20,13 +23,18 @@ class UserList(Resource):
         """Register a new user"""
         user_data = api.payload
 
-        # Simulate email uniqueness check (to be replaced by real validation with persistence)
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
 
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+    
+    @api.response(200, 'User list retrieved succesfully')
+    def get(self):
+        users = facade.get_all_users()
+        user_list = [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email} for user in users]
+        return user_list, 200
     
 @api.route('/<user_id>')
 class UserResource(Resource):
@@ -38,3 +46,26 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User information updated successfully')
+    @api.response(404, 'User not found')
+    def put(self, user_id):
+        user_data = api.payload
+        
+        try:
+            user_id = str(uuid.UUID(user_id))
+        except ValueError:
+            return {'error': 'Invalid user ID format'}, 400
+        
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        
+        updated_user = facade.update_user(user_id, user_data)
+        return {
+            'id': updated_user.id,
+            'first_name': updated_user.first_name,
+            'last_name': updated_user.last_name,
+            'email': updated_user.email
+        }, 200
