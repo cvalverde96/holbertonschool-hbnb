f51ee29bd3fd10c97af8352b168ebceeb933241a
cvalverde96 <cvalverde96@gmail.com>
2024-10-25 16:34:04 -0400

    subiendo user sin el reguero de comentarios

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
-from base import BaseModel
+from app.models.base import BaseModel
 import re
-"""
-importamos uuid para generar Universally Unique Identifiers para los usuarios
-importamos datetime para guardar cuando se crearon y fueron updated los usuarios
-importamos re para validar los formatos de email
-"""
 
 class User(BaseModel):
     # metodo constructor inicializando los atributos
@@ -16,24 +11,13 @@ class User(BaseModel):
         self.last_name = self.validate_name(last_name)
         self.email = self.validate_email(email)
         self.is_admin = is_admin
-        
     
-    
-    """
-    metodo estatico validate_name
-    para validar que nombre tenga valor/no este vacio
-    y que se largo no sea mayor de 50 chars
-    """
     @staticmethod
     def validate_name(name: str) -> str:
         if not name or len(name) > 50:
             raise ValueError("Name is required and/or maximum length is of 50 characters")
         return (name)
     
-    """
-    metodo estatico validate_email
-    para validar que email tenga formato requerido
-    """
     @staticmethod
     def validate_email(email : str) -> str:
         email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
@@ -41,12 +25,6 @@ class User(BaseModel):
             raise ValueError("Email is required and/or format is invalid")
         return (email)
     
-    
-    """
-    metodo para actualizar los datos de los usuarios
-    cada if es para validar que el formateo este correcto
-    y luego actualizarlo
-    """
     def update(self, first_name=None, last_name= None, email= None, is_admin= None):
         if first_name:
             self.first_name = self.validate_name(first_name)
