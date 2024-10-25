d31510a579030c0c7b9f55395572c3bacbbee7a5
cvalverde96 <cvalverde96@gmail.com>
2024-10-25 16:58:40 -0400

    subiendo el base para poder modificar los amenities

--- part2/app/models/base.py
+++ part2/app/models/base.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 
-class BaseModel():
+class BaseModel:
     def __init__(self):
         # Genera id unico para cada instancia
         self.id = str(uuid.uuid4())
@@ -15,8 +15,11 @@ class BaseModel():
         # actualiza updated_at con tiempo actual
         self.updated_at = datetime.now()
         
-    def update(self):
-        self.update_timestamp()
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.updated_timestamp()
     
     def __str__(self):
         return (f"BaseModel(id={self.id}, created_at={self.created_at}, updated_at={self.updated_at})")
