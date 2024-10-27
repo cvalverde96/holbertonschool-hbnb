953e9032f623e0ef2343566cf22c10a2072168a6
cvalverde96 <cvalverde96@gmail.com>
2024-10-27 11:59:56 -0400

    subiendo users con arreglo en imports

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -6,7 +6,7 @@ import uuid
 
 api = Namespace('users', description='User operations')
 
-# Define the user model for input validation and documentation
+
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
