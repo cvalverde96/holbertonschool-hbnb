5f6877dcc5bdaa51ec0beaac409df93e1540286d
cvalverde96 <cvalverde96@gmail.com>
2024-10-27 12:07:50 -0400

    arreglanod __init file para que este mas en acordecon pycodestlye

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -7,6 +7,7 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
+
 def create_app():
     app = Flask(__name__)
     api = Api(app, version="1.0", title="HBnB API", description="HBnB Application API")
@@ -15,4 +16,5 @@ def create_app():
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    
     return (app)
