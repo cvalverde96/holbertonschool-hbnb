917c137ca4166c27cfc55758a196c38e587776a9
cvalverde96 <cvalverde96@gmail.com>
2024-10-27 12:04:32 -0400

    subiendo review eliminando comentarios

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,69 +1,42 @@
 #!/usr/bin/python3
 
-from base import BaseModel
-from place import Place
-from user import User
-
-"""
-importamos uuid para crear el unique identifier
-importamos datetime para tener el tiempo exacto
-"""
+from app.models.base import BaseModel
+from app.models.place import Place
+from app.models.user import User
 
 
-class Review:
-    # metodo constructor inicializando los atributos
+class Review(BaseModel):
     def __init__(self, text: str, rating: int, place: "Place", user: "User"):
         super().__init__()
-        # atributos que llaman a sus respectivos validates para tener formato correcto
         self.text = self.validate_text(text)
         self.rating = self.validate_rating(rating)
         self.place = self.validate_place(place)
         self.user = self.validate_user(user)
-        # atributo para tener tiempo acutal en creacion/y update
     
-    """
-    metodo estatico para validar que en efecto se este poniendo texto en review
-    """
     @staticmethod
     def validate_text(text: str) -> str:
         if not text:
             raise ValueError("Text required")
         return (text)
     
-    
-    """
-    metodo estatico para validar que rating sea entre 1 y el 5
-    """
     @staticmethod
     def validate_rating(rating: int) -> int:
         if not (1 <= rating <= 5):
             raise ValueError("Rating given to the place, must be between 1 and 5.")
         return (rating)
     
-    
-    """
-    metodo estatico para validar que se este entrando una instancia valida de Place
-    """
     @staticmethod
     def validate_place(place: "Place") -> "Place":
         if not isinstance(place, Place):
             raise ValueError("Must be a valid Place instance")
         return (place)
     
-    
-    """
-    metodo estatico para validar que se este entrando una instancia valida de User
-    """
     @staticmethod
     def validate_user(user: "User") -> "User":
         if not isinstance(user, User):
             raise ValueError("Must be a valid User instance")
         return (user)
     
-    """
-    metodo updata para poder actutalizar la infromatcion del review
-    validaciones de if statements del texto y rating
-    """
     def update(self, text: str = None, rating: int = None):
         if text:
             self.text = self.validate_text(text)
