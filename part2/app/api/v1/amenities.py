bb73b95584a3f977fe8aa72759cccd1437958a24
cvalverde96 <cvalverde96@gmail.com>
2024-10-27 11:56:41 -0400

    subiendo amenitites con cambio en imports y correcion en logica

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from flask_restx import Namespace, Resource, fields
-from ...services.facade import HBnBFacade
+from app.services import HBnBFacade
 
 api = Namespace('amenities', description='Amenity operations')
 facade = HBnBFacade()
