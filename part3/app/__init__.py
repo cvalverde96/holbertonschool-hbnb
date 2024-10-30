7a1d25a54ac17af2d8fe643208d0d64400a5a9bf
cvalverde96 <cvalverde96@gmail.com>
2024-10-30 13:16:24 -0400

    subiendo init file de app con bcrypt incluido

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,11 +2,13 @@
 
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
+bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -18,5 +20,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-
+    
+    bcrypt.init_app(app)
+    
     return (app)
